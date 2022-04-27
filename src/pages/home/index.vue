639d9d642af088fad8817e9223be1677888bade1
黄丽珊 <huanglishan@qdama.cn>
2022-04-27 11:17:17 +0800

    1

---
+++ src/pages/home/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    home页面
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {}
+  }
+}
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/pages/trip/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    关于我的行程
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {}
+  }
+}
+
+</script>
+
+<style scoped>
+
+</style>
